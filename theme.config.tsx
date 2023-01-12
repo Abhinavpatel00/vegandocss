dc42994f284dce8c36a303015e08722d21c7ecf9
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-13 02:25:01 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/',
   },
   chat: {
     link: 'https://discord.com',
