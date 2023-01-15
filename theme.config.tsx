31c73f08360cdc9eb1802f4c9254ff35a7e06dcb
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-15 14:21:23 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,60 +1,46 @@
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
-import { NextSeo } from 'next-seo'
- 
-const Head = () => {
-  const { asPath } = useRouter()
-  const { frontMatter } = useConfig()
-  return (
-    <>
-      <NextSeo 
-        titleTemplate="%s – Vegn Documentation " 
-        openGraph={{
-          url: https://vegandocss.vercel.app/ ${asPath},
-          title: frontMatter.title || 'Vegan Documentation',
-          description: frontMatter.description || ' '
-        }}
-      />
-    </>
-  )
-}
- 
-export default Head
 
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/',
-  footer: {
-    text: 'Vegan docs',
-  },
-}
-
-export default {
-  useNextSeoProps() {
-    return {
-      titleTemplate: '%s – Vegn Documentation '
+export const useNextSeoProps = () => {
+    const { route } = useRouter()
+    if (route !== '/') {
+        return {
+            titleTemplate: '%s – Vegan docs'
+        }
     }
-  },
 }
 
-export default {
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Vegan Doc " />
-      <meta property="og:description" content="" />
+export const head = () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+    return <>
+        <meta property="og:url" content={https://vegandocss.vercel.app${asPath}} />
+        <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+        <meta property="og:description" content={frontMatter.description || 'The Vegan documentation '} />
     </>
-  ),
 }
+
+export const logo = (
+    <>
+        <svg width="24" height="24" viewBox="0 0 24 24">
+            <path
+                fill="currentColor"
+                d="M14.683 14.828a4.055 4.055 0 0 1-1.272.858a4.002 4.002 0 0 1-4.875-1.45l-1.658 1.119a6.063 6.063 0 0 0 1.621 1.62a5.963 5.963 0 0 0 2.148.903a6.035 6.035 0 0 0 3.542-.35a6.048 6.048 0 0 0 1.907-1.284c.272-.271.52-.571.734-.889l-1.658-1.119a4.147 4.147 0 0 1-.489.592z M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2zm0 2c2.953 0 5.531 1.613 6.918 4H5.082C6.469 5.613 9.047 4 12 4zm0 16c-4.411 0-8-3.589-8-8c0-.691.098-1.359.264-2H5v1a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2h2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-1h.736c.166.641.264 1.309.264 2c0 4.411-3.589 8-8 8z"
+            />
+        </svg>
+        <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+           Vegan docs
+        </span>
+    </>
+)
+
+const themeConfig = {
+    useNextSeoProps,
+    head,
+    logo
+}
+
+export default themeConfig;
 
 
 
