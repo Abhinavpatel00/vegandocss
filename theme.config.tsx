eca15419c47135cf37a310e3aa2d4cf718f1a6b6
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-15 13:10:09 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -28,10 +28,32 @@ export default {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Vegan Doc " />
-      <meta property="og:description" content="Veganism the way to spread kindness" />
+      <meta property="og:description" content="" />
     </>
   ),
 }
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+import { NextSeo } from 'next-seo'
+ 
+const Head = () => {
+  const { asPath } = useRouter()
+  const { frontMatter } = useConfig()
+  return (
+    <>
+      <NextSeo 
+        titleTemplate="%s – SWR" 
+        openGraph={{
+          url: https://vegandocss.vercel.app/ ${asPath},
+          title: frontMatter.title || 'Vegan Docs',
+          description: frontMatter.description || ' '
+        }}
+      />
+    </>
+  )
+}
+ 
+export default Head
 
 
 
