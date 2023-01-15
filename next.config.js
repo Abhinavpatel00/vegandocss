493b9d443c496537bfad594b40f0a84e06f89f17
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-15 14:26:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra()
 
-const withNextra = require('nextra')({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-})
+
  
 module.exports = withNextra({
   i18n: {
