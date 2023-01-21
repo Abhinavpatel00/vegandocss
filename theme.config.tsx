5ad8c2cd17ec1b9d30af6b0c2305e1a5c984f83b
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-21 21:11:31 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,7 +18,7 @@ export default {
   ),
   head: (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      
       <meta name="description" content="Vegan docs" />
       <meta name="og:title" content="vegan docs" />
     </>
