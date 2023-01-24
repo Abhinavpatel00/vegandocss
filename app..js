d73f92da99e16b23e8fa6f2603ea2d21f5d9260e
guglu-paglu <121427766+guglu-paglu@users.noreply.github.com>
2023-01-24 21:10:04 +0530

    Update app..js

--- app..js
+++ app..js
@@ -1,2 +1,17 @@
 
 import SEO from 'next-seo';
+
+const config = {
+  title: 'Vegn Docs',
+  description: 'My website description',
+  openGraph: {
+    type: 'website',
+    url: '',
+    site_name: 'Vegan Documentation ',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
